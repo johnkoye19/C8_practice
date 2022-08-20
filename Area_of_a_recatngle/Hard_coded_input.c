fba2f6f94621bb153051b857ce4e092f342124a6
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-08-20 15:50:19 +0100

    Create Hard_coded_input.c

---
+++ Area_of_a_recatngle/Hard_coded_input.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - area with hard coded inputs
+* Return: returns 0(sucess)
+*/
+int main(void)
+{
+    float Len = 4;
+    float width = 5;
+    
+    float Area = Len * width;
+    printf("Area is %f square units", Area);
+    
+    return (0);
+}
