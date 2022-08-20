efdcf2f4a01badc497025b7e4a136041c92c02f1
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-08-20 16:03:19 +0100

    Create simple_interest.c

---
+++ simple_interest.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() 
+{
+    // Write C code here
+    /**
+     * simple interest, A is equal to
+     * P(1 + rt)
+     * P is the starting amount
+     * r is the rate
+     * t is the time
+     */
+    //printf("Hello world");
+    float A, P = 320, r = 1.5, t = 3;
+    
+    A = P * (1 + (r *t));
+    printf("For a sum of %f dollars, rate of %f per annum and time of %f years", P, r, t);
+    printf(", Simple interest is %f dollars", A - P);
+
+    return 0;
+}
