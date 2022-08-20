cd4dd5b8a08632784eeef2d79dbe537d168ad212
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-08-20 16:01:15 +0100

    Create Celcius_to_Fahrenheit .c

---
+++ Celcius_to_Fahrenheit .c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    /**
+     * celcuis to Fahrenhei
+     */
+    float c, a = 30;
+     
+    //printf("Hello world");
+    c = (1.8 * a) + 32;
+    printf("%f degrees is equal to %f Fahrenheit", a, c);
+
+    return (0);
+}
