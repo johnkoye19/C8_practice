ffa6e51c5cbc5c0651c2a5ae20bf431577e59abe
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-08-20 15:51:24 +0100

    Create User_inputs.c

---
+++ Area_of_a_recatngle/User_inputs.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - area with hard coded inputs
+* Return: returns 0(sucess)
+*/
+int main(void)
+{
+    float Len, width;
+    
+    printf("Do you want to calculate the area of a rectangle?\n");
+    printf("Type in Length followed by the Width: X,Y\n");
+    printf("No spaces!\n");
+    scanf("%f,%f", &Len, &width);
+    float Area = Len * width;
+    printf("Area is %f square units", Area);
+
+    return (0);
+}
