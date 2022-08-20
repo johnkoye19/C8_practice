2b88c56f183549e411274f866b369f896fe52e95
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-08-20 15:56:22 +0100

    Create cylinder_volume.c

---
+++ cylinder_volume.c
@@ -0,0 +1,17 @@
+/**
+* main - area with hard coded inputs
+* Return: returns 0(sucess)
+*/
+int main(void)
+{
+    const float pi = 3.142;
+    float r = 4, h = 5, CylinderV/*CircleA*/;
+    
+    /*CircleA = pi * r * r;*/
+    CylinderV = pi * r * r * h;
+    
+    /*printf("The Area of the circle is %f square units", CircleA);*/
+    printf("The volume of tge cylinder is %f cubic units", CylinderV);
+    
+    return (0);
+}
